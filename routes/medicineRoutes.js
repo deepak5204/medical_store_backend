4fead494ca2106920d5f8ac0ad8219c87a7676a2
deepak <pdeepak1255@gmail.com>
2026-02-13 14:46:54 +0530

    write swagger comments for deleteMedicine, updateMedicine, getMedicineById

--- routes/medicineRoutes.js
+++ routes/medicineRoutes.js
@@ -6,6 +6,37 @@ import medicineController from '../controllers/medicineController.js';
 import authMiddleware from '../middleware/authMiddleware.js';
 import authorizeRoles  from '../middleware/roleMiddleware.js';
 
+/**
+ * @swagger
+ * /api/medicines:
+ *   post:
+ *     summary: Add a new medicine
+ *     tags: [Medicines]
+ *     security:
+ *       - bearerAuth: []
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/Medicine'
+ *     responses:
+ *       201:
+ *         description: Medicine created successfully
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/Medicine'
+ *       400:
+ *         description: Validation error
+ *       401:
+ *         description: Unauthorized - Token missing or invalid
+ *       403:
+ *         description: Forbidden - Admin access required
+ *       500:
+ *         description: Internal server error
+ */
+
 router.post("/", authMiddleware, authorizeRoles("admin"), validate(medicineSchema), medicineController.addMedicine);
 
 /**
@@ -35,8 +66,98 @@ router.get("/pagination", medicineController.getMedicinesWithPagination);
 router.get("/search", medicineController.searchMedicine);
 
 
+/**
+ * @swagger
+ *     /api/medicines/{id}:
+ *     get:
+ *          summary: Get medicine by ID
+ *          tags: [Medicines]
+ *          parameters:
+ *              - in: path
+ *                name: id
+ *                required: true
+ *                description: Medicine ID
+ *                schema:
+ *                    type: string
+ *          responses:
+ *              200:
+ *                  description: Medicine details retrieved successfully
+ *              404:
+ *                  description: Medicine not found
+ *              500:
+ *                  description: Internal server error
+ */
 router.get("/:id", medicineController.getMedicineById);
+
+
+/**
+ * @swagger
+ * /api/medicines/{id}:
+ *  put:
+ *     summary: Update medicine by ID
+ *     tags: [Medicines]
+ *     security:
+ *       - bearerAuth: []
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         description: Medicine ID
+ *         schema:
+ *           type: string
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/Medicine'
+ *     responses:
+ *       200:
+ *         description: Medicine updated successfully
+ *       400:
+ *         description: Validation error
+ *       401:
+ *         description: Unauthorized
+ *       403:
+ *         description: Forbidden - Admin only
+ *       404:
+ *         description: Medicine not found
+ *       500:
+ *         description: Internal server error
+ */
+
 router.put("/:id", authMiddleware, authorizeRoles("admin"), medicineController.updateMedicine);
+
+
+/**
+ * @swagger
+ * /api/medicines/{id}:
+ *      delete:
+ *          summary: Delete medicine by ID
+ *          tags: [Medicines]
+ *          security:
+ *              - bearerAuth: []
+ *          parameters:
+ *            - in: path
+ *              name: id
+ *              required: true
+ *              description: Medicine ID
+ *              schema:
+ *                  type: string
+ *          responses:
+ *              200:
+ *                  description: Medicine deleted successfully
+ *              400:
+ *                  description: Validation error
+ *              401:
+ *                  description: Unauthorized
+ *              403:
+ *                  description: Forbidden - Admin only
+ *              404:
+ *                  description: Medicine not found
+ *              500:
+ *                  description: Internal server error
+ */
 router.delete("/:id", authMiddleware, authorizeRoles("admin"), medicineController.deleteMedicine);
 
 
